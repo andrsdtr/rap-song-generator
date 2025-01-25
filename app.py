6207d6e69367200c80f784ebc7a3156e95487130
andrsdtr <andreas-dichter@gmx.de>
2025-01-25 19:03:11 +0200

    adding wiki research

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain, SequentialChain
 from langchain.memory import ConversationBufferMemory
+from langchain.utilities import WikipediaAPIWrapper
 
 # for album cover
 import requests
@@ -19,9 +20,9 @@ title_template = PromptTemplate(
 )
 
 songtext_template = PromptTemplate(
-    input_variable = ['title'],
+    input_variable = ['title', 'wiki_research'],
     template= """
-        Write a rap song based on this title: {title}. Use markdown formatting with the following rules:
+        Write a rap song based on this title: {title} while leveraging this wikipedia research: {wiki_research}. Use markdown formatting with the following rules:
         1. Each verse is preceded by `### Verse` followed by a space.
         2. Each chorus is preceded by `### Chorus` followed by a space.
         3. Insert a blank line after each line of the rap for proper markdown rendering.
@@ -46,13 +47,17 @@ songtext_template = PromptTemplate(
     #'Write a rap song based on this title: {title}. Format the text using Markdown. Structure the song with verses and choruses, marking each with ### followed by a single space. Ensure every line ends with \\n Here is an example of the structure: ### Verse 1\\n Line one of the verse.\\n Line two of the verse.\\n ### Chorus\\n First line of the chorus.\\n Second line of the chorus.\\n')
 
 # Memory
-memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+title_memory = ConversationBufferMemory(input_key='topic', memory_key='chat_history')
+songtext_memory = ConversationBufferMemory(input_key='title', memory_key='chat_history')
 
 # LLMs and Chains
 llm = OpenAI(temperature=0.9)
-title_chain = LLMChain(llm=llm, prompt=title_template, output_key='title', memory=memory, verbose=True) #verbose=True prints prompts
-songtext_chain = LLMChain(llm=llm, prompt=songtext_template, output_key='songtext', memory=memory, verbose=True)
-sequential_chain = SequentialChain(chains=[title_chain, songtext_chain], input_variables=['topic'], output_variables=['title', 'songtext'], verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_template, output_key='title', memory=title_memory, verbose=True) #verbose=True prints prompts
+songtext_chain = LLMChain(llm=llm, prompt=songtext_template, output_key='songtext', memory=songtext_memory, verbose=True)
+
+wiki = WikipediaAPIWrapper()
+
+# sequential_chain = SequentialChain(chains=[title_chain, songtext_chain], input_variables=['topic'], output_variables=['title', 'songtext'], verbose=True)
 
 # #App framework
 st.title('👻 Rap Song Creator')
@@ -69,13 +74,22 @@ def generate_cover(title):
 
 # # Show response
 if prompt:
-    response = sequential_chain({'topic':prompt})
+    title = title_chain.run(prompt)
+    wiki_research = wiki.run(prompt)
+    songtext = songtext_chain.run(title=title, wiki_research=wiki_research)
+    #response = sequential_chain({'topic':prompt})
     #cover = generate_cover(response['title'])
     #print(cover)
-    print(response)
-    st.markdown(response['title'])
-    st.markdown(response['songtext'])
+    #print(response)
+    st.markdown(title)
+    st.markdown(songtext)
     #st.image(cover)
 
-    with st.expander('Message History'):
-        st.info(memory.buffer)
+    with st.expander('Title History'):
+        st.info(title_memory.buffer)
+
+    with st.expander('Songtext History'):
+        st.info(songtext_memory.buffer)
+
+    with st.expander('Wikpedia Research History'):
+        st.info(wiki_research)
